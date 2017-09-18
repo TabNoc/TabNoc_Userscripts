dfb608d1a3b56931d13c403ca57deaf33cae131e
mnpingpong <meinich.spiel@gmail.com>
2017-09-18 14:54:33 +0200

    Update States.js

--- base/States.js
+++ base/States.js
@@ -1,72 +1,104 @@
-//TODO: begrenzung der Anzahl an States
-//TODO: requestState non numeric
-
-function AddState(oldState, newState, changes) {
-	maxAmount = 10
+function AddState(oldState, newState, changes, maxAmount = 10) {
 	if (oldState == null) {
-		throw new Error("oldState not provided");
+		throw new Error("NullPointerExeption: oldState not provided");
 	}
 	if (newState == null) {
-		throw new Error("newState not provided");
+		throw new Error("NullPointerExeption: newState not provided");
 	}
 	if (changes == null) {
-		throw new Error("changes not provided");
+		throw new Error("NullPointerExeption: changes not provided");
 	}
-	if (changes.firstItem == null) {
-		changes.firstItem = 0;
+	if (maxAmount == null) {
+		maxAmount = 10;
+	}
+	if (maxAmount < 2) {
+		throw new Error("OutOfRangeException: maxAmount must be greater than 1");
+	}
+	if (changes.changeLog == null) {
+		changes.changeLog = ([]);
 	}
 	if (changes.data == null) {
-		changes.data = ([]);
+		changes.data = ({});
 	}
-	changes.lastItem = changes.data.push(jsondiffpatch.diff(oldState, newState));
-	changes.firstItem = Math.abs(changes.firstItem - changes.lastItem) > maxAmount ? changes.lastItem - maxAmount : changes.firstItem;
-	
-	changes.data.splice(0, changes.firstItem - 1);
+	var time = (new Date).getTime();
+	let appendIndex = "";
+
+	while (changes.changeLog.indexOf(time + appendIndex) != -1) {
+		appendIndex = appendIndex == "" ? "-1" : (--appendIndex).toString();
+	}
+	changes.changeLog.push(time + appendIndex);
+	changes.data[time + appendIndex] = jsondiffpatch.diff(oldState, newState);
+
+	while (changes.changeLog.length > maxAmount) {
+		delete changes.data[changes.changeLog.splice(0, 1)[0]];
+		changes.removedChangeLogEntries++;
+	}
+	return ({StateTime:time + appendIndex, StateNr:changes.changeLog.length - 1});
 }
 
-function GetState(currentState, changes, requestedStateNr) {
+function GetState(currentState, changes, requestedStateNr, requestAsTime) {
 	if (currentState == null) {
-		throw new Error("currentState not provided");
+		throw new Error("NullPointerExeption: currentState not provided");
 	}
 	if (changes == null) {
-		throw new Error("changes not provided");
+		throw new Error("NullPointerExeption: changes not provided");
 	}
 	if (requestedStateNr == null) {
-		throw new Error("requestedStateNr not provided");
+		throw new Error("NullPointerExeption: requestedStateNr not provided");
 	}
-	currentStateNr = changes.length;
+	if (requestedStateNr < 0) {
+		throw new Error("OutOfRangeException: requestedStateNr is less than 0");
+	}
+
+	currentStateNr = changes.changeLog.length + changes.removedChangeLogEntries;
 	if (currentStateNr == requestedStateNr) {
 		return currentState;
 	}
+
+	if (requestAsTime === true) {
+		requestedStateNr = changes.changeLog.indexOf(requestedStateNr);
+		if (requestedStateNr == -1) {
+			throw new Error("OutOfRangeException: The provided Timestamp requestedStateNr does not exists in changeLog");
+		}
+	}
+
 	if (currentStateNr < requestedStateNr) {
-		throw new Error("");
+		throw new Error("OutOfRangeException: requestedStateNr is in the future");
 	}
 	workingState = eval(currentState.toSource());
 	for (workingStateNr = currentStateNr - 1; workingStateNr >= requestedStateNr; workingStateNr--) {
-		workingState = jsondiffpatch.unpatch(workingState, changes.data[workingStateNr]);
+		workingState = jsondiffpatch.unpatch(workingState, changes.data[changes.changeLog[workingStateNr]]);
 	}
 	return workingState;
 }
 
 function test() {
-	changes = ({data:[]});
-	currentState = ({Baum:"tree"});
-	oldState = currentState.toSource();
-	for (i = 1; i < 1000; i++) {
-		oldState = currentState.toSource();
-		currentState[Math.random()] = Math.random();
-		currentState["State"] = i;
-		AddState(eval(oldState), currentState, changes);
+	changes = ({});
+	testState = ({Baum:"tree"});
+	for (i = 1; i < 30; i++) {
+		let oldState = testState.toSource();
+		testState[i] = "!";
+		testState["State"] = i;
+		// console.log("State", i, ":", eval(oldState), ">", currentState);
+		if (AddState(eval(oldState), testState, changes, 10000).StateNr != i) {
+			new Error("returned StateNr not expected Statenr")
+		}
 	}
-	// console.info(changes);
-	// currentState = i;
-	// for (i = 1; i < 999; i++) {
-		// result = GetState(currentState, changes, i);
-		// if (result.State != i) {
-			// throw new Error("testState wrong");
-		// }
-	// }
-	return currentState;
+	console.info(changes);
+	for (i = 1; i < 30; i++) {
+		let result = GetState(testState, changes, i);
+		if (result.State != i) {
+			console.warn(i, result.State);
+			throw new Error("testState wrong");
+		}
+	}
+	return testState;
 }
 
-testState = test();
+MyTestState = test();
+if (GetState(MyTestState, changes, 2).State != 2){
+	console.error("test Failed!");
+}
+else {
+	console.info("test passed!")
+}
