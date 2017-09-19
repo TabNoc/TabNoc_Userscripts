2f38dffd3be431e654e338eddfedaa3a5c9dea00
mnpingpong <meinich.spiel@gmail.com>
2017-09-19 11:57:26 +0200

    Update States.js

--- base/States.js
+++ base/States.js
@@ -1,3 +1,4 @@
+// @maxAmount: Anzahl an States
 function AddState(oldState, newState, changes, maxAmount = 10) {
 	if (oldState == null) {
 		throw new Error("NullPointerExeption: oldState not provided");
@@ -20,6 +21,9 @@ function AddState(oldState, newState, changes, maxAmount = 10) {
 	if (changes.data == null) {
 		changes.data = ({});
 	}
+	if (changes.removedChangeLogEntries == null) {
+		changes.removedChangeLogEntries =0;
+	}
 	var time = (new Date).getTime();
 	let appendIndex = "";
 
@@ -31,9 +35,9 @@ function AddState(oldState, newState, changes, maxAmount = 10) {
 
 	while (changes.changeLog.length > maxAmount) {
 		delete changes.data[changes.changeLog.splice(0, 1)[0]];
-		changes.removedChangeLogEntries++;
+		changes.removedChangeLogEntries = changes.removedChangeLogEntries + 1;
 	}
-	return ({StateTime:time + appendIndex, StateNr:changes.changeLog.length - 1});
+	return ({StateTime:time + appendIndex, StateNr:changes.changeLog.length + changes.removedChangeLogEntries});
 }
 
 function GetState(currentState, changes, requestedStateNr, requestAsTime) {
@@ -50,10 +54,7 @@ function GetState(currentState, changes, requestedStateNr, requestAsTime) {
 		throw new Error("OutOfRangeException: requestedStateNr is less than 0");
 	}
 
-	currentStateNr = changes.changeLog.length + changes.removedChangeLogEntries;
-	if (currentStateNr == requestedStateNr) {
-		return currentState;
-	}
+	var currentStateNr = changes.changeLog.length + changes.removedChangeLogEntries;
 
 	if (requestAsTime === true) {
 		requestedStateNr = changes.changeLog.indexOf(requestedStateNr);
@@ -62,43 +63,54 @@ function GetState(currentState, changes, requestedStateNr, requestAsTime) {
 		}
 	}
 
+	if (currentStateNr == requestedStateNr) {
+		return currentState;
+	}
+
 	if (currentStateNr < requestedStateNr) {
 		throw new Error("OutOfRangeException: requestedStateNr is in the future");
 	}
-	workingState = eval(currentState.toSource());
+
+	if (requestedStateNr <  changes.removedChangeLogEntries) {
+		throw new Error("OutOfRangeException: requestedStateNr was already deleted");
+	}
+
+	var workingState = eval(currentState.toSource());
 	for (workingStateNr = currentStateNr - 1; workingStateNr >= requestedStateNr; workingStateNr--) {
-		workingState = jsondiffpatch.unpatch(workingState, changes.data[changes.changeLog[workingStateNr]]);
+		let diffData = changes.data[changes.changeLog[workingStateNr - changes.removedChangeLogEntries]];
+		if (diffData == null) {
+			throw new Error("NullPointerExeption: diffData is null [workingStateNr:" + workingStateNr + "]")
+		}
+		workingState = jsondiffpatch.unpatch(workingState, changes.data[changes.changeLog[workingStateNr - changes.removedChangeLogEntries]]);
 	}
 	return workingState;
 }
 
 function test() {
-	changes = ({});
-	testState = ({Baum:"tree"});
-	for (i = 1; i < 30; i++) {
+	let changes = ({});
+	let testState = ({Baum:"tree"});
+	let testCount = 30;
+	for (i = 1; i <= testCount; i++) {
 		let oldState = testState.toSource();
 		testState[i] = "!";
 		testState["State"] = i;
 		// console.log("State", i, ":", eval(oldState), ">", currentState);
-		if (AddState(eval(oldState), testState, changes, 10000).StateNr != i) {
+		if (AddState(eval(oldState), testState, changes, testCount - 2).StateNr != i) {
 			new Error("returned StateNr not expected Statenr")
 		}
 	}
-	console.info(changes);
-	for (i = 1; i < 30; i++) {
+	try {
+		GetState(testState, changes, 0);
+		GetState(testState, changes, 1);
+		console.error("Test 1 failed!");
+	}
+	catch (e) {}
+	for (i = 2; i <= testCount; i++) {
 		let result = GetState(testState, changes, i);
-		if (result.State != i) {
-			console.warn(i, result.State);
-			throw new Error("testState wrong");
+		if (result.State != i || Object.keys(result).length != i + 2) {
+			console.warn(i, result);
+			throw new Error("Test 2 failed!");
 		}
 	}
 	return testState;
-}
-
-MyTestState = test();
-if (GetState(MyTestState, changes, 2).State != 2){
-	console.error("test Failed!");
-}
-else {
-	console.info("test passed!")
 }
