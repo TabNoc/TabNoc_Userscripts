c1a25367731da227fbc2110c2dd6c52008cd2d09
TabNoc <meinich.spiel@gmail.com>
2018-05-17 11:19:33 +0200

    MarkGolemPages - v1.3.6
    17.05.2018 - 1.3.6
    	- added support for mobile Devices
    	- fixed: UpdateDatabaseTable CheckInit

--- Golem/MarkGolemPages.user.js
+++ Golem/MarkGolemPages.user.js
@@ -2,7 +2,7 @@
 // @name        MarkGolemPages
 // @namespace   TabNoc
 // @include     http*://www.golem.de/*
-// @version     1.3.5_14052018
+// @version     1.3.6_17052018
 // @require     https://code.jquery.com/jquery-2.1.1.min.js
 // @require     https://raw.githubusercontent.com/mnpingpong/TabNoc_Userscripts/ImplementSync/base/GM__.js
 // @require     https://raw.githubusercontent.com/mnpingpong/TabNoc_Userscripts/ImplementSync/base/TabNoc.js
@@ -83,6 +83,10 @@ Start Writing Script
 
 14.05.2018 - 1.3.5
 	- added: scrolling on NewsPage with < and >
+
+17.05.2018 - 1.3.6
+	- added support for mobile Devices
+	- fixed: UpdateDatabaseTable CheckInit
 */
 
 try {
@@ -122,12 +126,12 @@ try {
 			ShowOpenInNewTabButton: false,
 
 			ScanButtonDomParent: "#grandwrapper",
-			ElementsSearchString: "#index-promo, .list-articles>li",
+			ElementsSearchString: (MobileCheck() ? ".leader, .media__teaser-list>li>div" : "#index-promo, .list-articles>li"),
 			NameOfElements: "Newspages",
 			NameOfElement: "Newspage",
 			GetIDFunction: function(element) {return $(element).children("a")[0].getAttribute("href");},
 			GetCurrentSiteIDFunction: function() {return document.URL;},
-			CheckCurrentElementFunction: function(element) {return $(element).children("a").length === 1 && ($(element).children("a")[0].getAttribute("id").includes("hpalt") ||
+			CheckCurrentElementFunction: function(element) {return $(element).children("a").length === 1 && ($(element).children("a")[0].getAttribute("id").includes("hpal" + (MobileCheck() === false ? "t" : "")) ||
 														  $(element).children("a")[0].getAttribute("id").includes("bigalt") ||
 														  $(element).children("a")[0].getAttribute("id").includes("msalt"));}
 		},
@@ -138,6 +142,18 @@ try {
 			OpenTabButton: '<div class="MyOpenTabButton" title="Open in new Tab"><div></div></div>'
 		}
 	});
+
+	function MobileCheck() {
+		var check = false;
+		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	};
+
+	function MobileAndTabletCheck() {
+		var check = false;
+		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	};
 
 	// ### http*://www.golem.de ###
 	function StartPageLoader() {
@@ -157,31 +173,33 @@ try {
 
 			// ## ScanWithKeyPress ##
 
-			if (TabNoc.Settings.ScanWithKeyPressEnabled === true) {
-				$(document).on("keydown", event => {
-					if (event.keyCode == 160) {
-						if (TabNoc.Variables.ScanWithKeyPressActiveTimeout > new Date() && TabNoc.Variables.WasHidden == false) {
-							let scanElement = $(TabNoc.Settings.ElementsSearchString).toArray().find(element => element.className == "MyPageElement");
-							if (scanElement != null) {getAllElements(null, TabNoc.Settings.GetIDFunction(scanElement), false);}
-							event.preventDefault();
-							TabNoc.Variables.ScanWithKeyPressActiveTimeout = new Date(new Date().getTime() + 600000);
-						}
-						else {
-							if (confirm("Soll die Bearbeitung durch ScanWithKeyPress aktiviert werden?") == true) {
-							TabNoc.Variables.ScanWithKeyPressActiveTimeout = new Date(new Date().getTime() + 600000);
-								TabNoc.Variables.WasHidden = false;
+			if (MobileCheck() === false) {
+				if (TabNoc.Settings.ScanWithKeyPressEnabled === true) {
+					$(document).on("keydown", event => {
+						if (event.keyCode == 160) {
+							if (TabNoc.Variables.ScanWithKeyPressActiveTimeout > new Date() && TabNoc.Variables.WasHidden == false) {
+								let scanElement = $(TabNoc.Settings.ElementsSearchString).toArray().find(element => element.className == "MyPageElement");
+								if (scanElement != null) {getAllElements(null, TabNoc.Settings.GetIDFunction(scanElement), false);}
+								event.preventDefault();
+								TabNoc.Variables.ScanWithKeyPressActiveTimeout = new Date(new Date().getTime() + 600000);
+							}
+							else {
+								if (confirm("Soll die Bearbeitung durch ScanWithKeyPress aktiviert werden?") == true) {
+									TabNoc.Variables.ScanWithKeyPressActiveTimeout = new Date(new Date().getTime() + 600000);
+									TabNoc.Variables.WasHidden = false;
+								}
 							}
 						}
-					}
-				});
+					});
+				}
 			}
 
 			// ## ScanWithKeyPress ##
-
-			var ScanButton = $(TabNoc.HTML.ScanButton);
-			ScanButton.click(function(){getAllElements();$(TabNoc.Settings.ScanButtonDomParent + ">.MyScanButton").remove();});
-			$(TabNoc.Settings.ScanButtonDomParent).append(ScanButton);
-
+			if (MobileCheck() === false) {
+				var ScanButton = $(TabNoc.HTML.ScanButton);
+				ScanButton.click(function(){getAllElements();$(TabNoc.Settings.ScanButtonDomParent + ">.MyScanButton").remove();});
+				$(TabNoc.Settings.ScanButtonDomParent).append(ScanButton);
+			}
 			var ValueChangeCallback = function(name, old_value, new_value, remote) {
 				startCheckElements(TabNoc.Variables.MarkToggleState, true);
 			};
@@ -286,11 +304,12 @@ try {
 			ScanButton.click(function(){getAllElements(SearchString, SearchString);});
 			$(checkElement).append(ScanButton);
 
-			let ReadButton = $(TabNoc.HTML.ReadButton);
-			ReadButton.click(function(){if (confirm("Soll dieser Eintrag wirklich ohne zu öffnen auf ToRead gesetz werden?") == true) getAllElements(SearchString, SearchString, true);});
-			$(checkElement).append(ReadButton);
-
-			if (TabNoc.Settings.ShowOpenInNewTabButton == true) {
+			if (MobileCheck() === false) {
+				let ReadButton = $(TabNoc.HTML.ReadButton);
+				ReadButton.click(function(){if (confirm("Soll dieser Eintrag wirklich ohne zu öffnen auf ToRead gesetz werden?") == true) getAllElements(SearchString, SearchString, true);});
+				$(checkElement).append(ReadButton);
+			}
+			if (TabNoc.Settings.ShowOpenInNewTabButton == true && MobileCheck() === false) {
 				let OpenTabButton = $(TabNoc.HTML.OpenTabButton);
 				OpenTabButton.click(function(){GM_openInTab(SearchString, {active: false, loadInBackground: true, insert: true, setParent: true});});
 				$(checkElement).append(OpenTabButton);
@@ -651,7 +670,7 @@ try {
 		if (CurrentVersionNumber != ExpectedVersionNumber) {
 			let updateMsg = "Es wurde ein Versionsunterschied der Datenbank-Tabelle " + TableName + " gefunden (aktuell: " + CurrentVersionNumber + " | erwartet: " + ExpectedVersionNumber + ")";
 			console.info(updateMsg);
-			if (TableUpdateObject.CheckInit() === false) {
+			if (TableUpdateObject[TableName].CheckInit() === false) {
 				alert(updateMsg + "\r\nOK drücken um den Updatevorgang zu starten.");
 				if (TableUpdateObject[TableName][CurrentVersionNumber] == null) {
 					throw ("NoUpdateImplemeneted:\r\nFür die Version " + CurrentVersionNumber + " der Tabelle " + TableName + " wurde kein Update definiert!");
@@ -674,7 +693,7 @@ try {
 				}
 			}
 			else {
-				TableUpdateObject.InitTable();
+				TableUpdateObject[TableName].InitTable();
 				GM_setValue(TableName + "-Version", ExpectedVersionNumber);
 				let updateMsg = "Die Datenbank-Tabelle " + TableName + " wurde initialisiert (Version: " + GM_getValue(TableName + "-Version") + ")";
 				console.log(updateMsg);
